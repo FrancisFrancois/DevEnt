0632ebda31c3f674dae8722c865df8b5309e8f2d
FrancisFrancois <francisfrancois9189@gmai.com>
2023-09-12 14:08:52 +0200

    Mise en place du HTML login registration et du footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ModelsComponent } from './shared/models/models.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ModelsComponent,
+    LoginComponent,
+    EventsComponent,
+    RegistrationComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
